0e775d97b2314142bc3a73d61de2ef3d470348e6
gianluca messinese <gianluca messinese>
2015-12-18 21:11:02 +0100

    Make insertSign() js function

--- myjs.js
+++ myjs.js
@@ -1,3 +1,21 @@
+var PlayerTurn = false;
+
+function checkStatus() {
+	
+}
+
+function insertSign() {
+	if (PlayerTurn) {
+		PlayerTurn = false;
+		return "O";
+	}
+	else {
+		PlayerTurn = true;
+	    return "X";
+	}
+	checkStatus();
+}
+
 function resetFields() {
 	var y;
 	for(var x = 1; x < 10; x++) {
