668e1503fb17482d4aa705b799e0c20fcfbb2a32
gianluca messinese <gianluca messinese>
2015-12-19 17:21:55 +0100

    Comment the js code

--- myjs.js
+++ myjs.js
@@ -4,6 +4,7 @@ var counter = 0; //count the number of sign(if nine, mathc is par)
 var score = [0, 0] //contains the score 
 var lastsign;
 
+//ligh the tag <p> of player who has to put the sign
 function  lightPlayer() {
 	if(PlayerTurn) {
 		document.getElementById("title2").style.backgroundColor = "#00FF00";
@@ -15,12 +16,14 @@ function  lightPlayer() {
 	}
 }
 
+//Invert the Player turn
 function invertPlayerTurn() {
 	if (PlayerTurn) PlayerTurn = false;
 	else PlayerTurn = true;
 	lightPlayer();
 }
 
+//Delete the last insertion
 function undo() {
 	if(lastsign !== null) {
 	    document.getElementById(lastsign).value = "";
@@ -32,6 +35,7 @@ function undo() {
 	lastsign = null;
 }
 
+//Check if the match is equal
 function checkPar() {
 	if(counter === 9) {
 		return true;
@@ -39,6 +43,7 @@ function checkPar() {
 	return false;
 }
 
+//Say the winner through an alert
 function sayWinner() {
 	if (PlayerTurn) {
 		return "The Winner is X";
@@ -46,12 +51,14 @@ function sayWinner() {
 	return "The Winner is O";
 }
 
+//Delete the signs stored into the array signs[]
 function deleteSigns() {
 	for(var i = 0; i < 9; i++) {
 	    signs[i] = "";
 	}
 }
 
+//increase the score 
 function updateScore() {
     if (PlayerTurn) {
 	    score[0]++;
@@ -63,10 +70,12 @@ function updateScore() {
 	}
 }
 
+//Block the button after his pression
 function blockButton(idbutton) {
 	document.getElementById(idbutton).disabled = true;
 }
 
+//Unlock all the buttons
 function unlockButtons() {
 	var i;
 	for (i = 0; i < 9; i++) {
@@ -74,6 +83,7 @@ function unlockButtons() {
 	}
 }
 
+//Check if a player has won
 function checkStatus() {
     if (
 	     ((signs[0] != "") && (signs[0] === signs[1]) && (signs[1] === signs[2])) ||
@@ -97,6 +107,7 @@ function checkStatus() {
 	}
 }
 
+//Insert the sign into the button 
 function insertSign(index) {
 	counter++;
 	if (PlayerTurn) {
@@ -114,6 +125,7 @@ function insertSign(index) {
 	checkStatus();
 }
 
+//reset the buttons' values
 function resetFields() {
 	var supp;
 	for(var i = 0; i < 9; i++) {
@@ -122,6 +134,7 @@ function resetFields() {
 	}
 }
 
+//start a new match 
 function startMatch() {
 	counter = 0;
 	resetFields();
@@ -130,9 +143,12 @@ function startMatch() {
 	lastsign = null;
 }
 
+//setScore to ""
 function setScore(player, index) {
     document.getElementById(player).innerHTML = score[index] + "";
 }
+
+//delete score and start a new match 
 function resetAll() {
 	score[0] = 0;
 	setScore("player1", 0);
