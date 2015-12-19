0acd68231181250361c8e6248d2937355c0a41af
gianluca messinese <gianluca messinese>
2015-12-19 01:06:18 +0100

    Add Checks

--- myjs.js
+++ myjs.js
@@ -1,7 +1,35 @@
 var PlayerTurn = false;
+var signs = ["", "", "" , "", "", "", "", "", ""];
+
+function sayWinner() {
+	if (PlayerTurn) {
+		return "The Winner is X";
+	}
+	return "The Winner is O";
+}
+
+function deleteSigns() {
+	for(var i = 0; i < 10; i++) {
+	    signs[i] = "";
+	}
+}
 
 function checkStatus() {
-	
+	if (
+	     ((signs[0] != "") && (signs[0] === signs[1]) && (signs[1] === signs[2])) ||
+		 ((signs[0] != "") && (signs[0] === signs[4]) && (signs[4] === signs[8])) ||
+		 ((signs[0] != "") && (signs[0] === signs[3]) && (signs[3] === signs[6])) ||
+		 ((signs[1] != "") && (signs[1] === signs[4]) && (signs[4] === signs[7])) ||
+		 ((signs[2] != "") && (signs[2] === signs[5]) && (signs[5] === signs[8])) ||
+		 ((signs[2] != "") && (signs[2] === signs[4]) && (signs[4] === signs[6])) ||
+		 ((signs[3] != "") && (signs[3] === signs[4]) && (signs[4] === signs[5])) ||
+		 ((signs[6] != "") && (signs[6] === signs[7]) && (signs[7] === signs[8]))
+	   )
+	{
+	    alert(sayWinner());
+		deleteSigns();
+		startMatch();
+	} 
 }
 
 function insertSign() {
@@ -13,14 +41,13 @@ function insertSign() {
 		PlayerTurn = true;
 	    return "X";
 	}
-	checkStatus();
 }
 
 function resetFields() {
-	var y;
-	for(var x = 1; x < 10; x++) {
-		y = x + "";
-        document.getElementById(y).value = "";
+	var supp;
+	for(var i = 1; i < 10; i++) {
+		supp = i + "";
+        document.getElementById(supp).value = "";
 	}
 }
 
