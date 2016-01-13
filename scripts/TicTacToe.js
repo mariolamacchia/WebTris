6a01fc5ec67fcf55758882f04da3725cc178916e
Mario Lamacchia <mario@appstrakt.com>
2016-01-13 23:07:48 +0100

    Create TicTacToe class

---
+++ scripts/TicTacToe.js
@@ -0,0 +1,135 @@
+if ( typeof define === "function" && define.amd ) {
+	define( "TicTacToe", [], function() {
+		return TicTacToe;
+	});
+}
+
+var TicTacToe = function() {
+  this.players = [];
+  this.isOpen = false;
+
+	// Add eventemitter for nodejs
+	if (!(document && document.createEvent)) {
+		this.eventEmitter = new require('events')();
+	}
+};
+
+TicTacToe.prototype.start = function() {
+  if (this.players.length !== 2) {
+    throw new Error('Invalid number of players. Must be exactly 2.');
+  }
+  this.turn = !Math.floor(Math.random()*2); //false = player1's turn, true = player2's turn
+  this.signs = ['', '', '', '', '', '', '', '', ''];
+  this.counter = 0; //count the number of sign(if nine, mathc is par)
+  this.isOpen = true;
+	this.emit('start');
+};
+
+TicTacToe.prototype.getCurrentPlayer = function() {
+  return this.players[1 * this.turn];
+};
+
+TicTacToe.prototype.getCell = function(x, y) {
+  return this.signs[y * 3 + x];
+};
+
+//Insert the sign into the button
+TicTacToe.prototype.insert = function(x, y) {
+  if (!this.isOpen) {
+    return false;
+  }
+  var index = y * 3 + x;
+  if (this.signs[index]) {
+    return false;
+  }
+  this.counter++;
+  if (this.turn) {
+    this.signs[index] = 'O';
+  } else {
+    this.signs[index] = 'X';
+  }
+	this.emit('fill', {
+		x: x,
+		y: y,
+		sign: this.signs[index]
+	});
+  this.lastsign = index;
+  this.checkStatus();
+  this.nextTurn();
+  return true;
+};
+
+//Delete the last insertion
+TicTacToe.prototype.undo = function() {
+  if(this.lastsign !== null) {
+    this.counter--;
+    this.signs[this.lastsign] = '';
+    this.nextTurn();
+    this.isOpen = true;
+		this.emit('unfill', {
+			x: this.lastsign % 3,
+			y: Math.floor(this.lastsign / 3)
+		});
+  	this.lastsign = null;
+    return true;
+	}
+  return false;
+};
+
+TicTacToe.prototype.nextTurn = function() {
+  this.turn = !this.turn;
+	this.emit('changeTurn', {player: this.getCurrentPlayer()});
+};
+
+//Check if a player has won
+TicTacToe.prototype.checkStatus = function() {
+  var signs = this.signs;
+  if (
+    ((signs[0] !== '') && (signs[0] === signs[1]) && (signs[1] === signs[2])) ||
+    ((signs[0] !== '') && (signs[0] === signs[4]) && (signs[4] === signs[8])) ||
+    ((signs[0] !== '') && (signs[0] === signs[3]) && (signs[3] === signs[6])) ||
+    ((signs[1] !== '') && (signs[1] === signs[4]) && (signs[4] === signs[7])) ||
+    ((signs[2] !== '') && (signs[2] === signs[5]) && (signs[5] === signs[8])) ||
+    ((signs[2] !== '') && (signs[2] === signs[4]) && (signs[4] === signs[6])) ||
+    ((signs[3] !== '') && (signs[3] === signs[4]) && (signs[4] === signs[5])) ||
+    ((signs[6] !== '') && (signs[6] === signs[7]) && (signs[7] === signs[8]))
+  ) {
+    this.win(this.getCurrentPlayer());
+  } else if (this.counter === 9) {
+    this.win();
+  }
+};
+
+TicTacToe.prototype.win = function(player) {
+  if (player) {
+    if (Number(player.score)) {
+      player.score++;
+    } else {
+      player.score = 1;
+    }
+		this.emit('win', {player: player});
+  }
+  this.isOpen = false;
+	this.emit('end', {player: player});
+};
+
+TicTacToe.prototype.emit = function(name, obj) {
+	if (this.eventEmitter) {
+		this.eventEmitter.emit(name, obj);
+	} else {
+		var e = document.createEvent('Event');
+		e.initEvent(name);
+		for (var k in obj) {
+			e[k] = obj[k];
+		}
+		document.body.dispatchEvent(e);
+	}
+};
+
+TicTacToe.prototype.on = function(name, fn) {
+	if (this.eventEmitter) {
+		this.eventEmitter.on(name, fn);
+	} else {
+		document.body.addEventListener(name, fn);
+	}
+};
--- scripts/myjs.js
+++ scripts/myjs.js
@@ -1,61 +1,47 @@
-var PlayerTurn = false; //false = player1's turn, true = player2's turn
-var signs = ['', '', '', '', '', '', '', '', ''];
-var counter = 0; //count the number of sign(if nine, mathc is par)
-var score = [0, 0]; //contains the score
-var lastsign;
-
 //Resize the botton in 1:1
 function resizeButtons() {
     var width;
     width = $('#0').css('width');
-    $('.field').css('height', width);    
+    $('.field').css('height', width);
 }
+
+var game = new TicTacToe();
+game.players.push({
+  id: 'player1',
+  name: 'Player 1',
+  score: 0
+}, {
+  id: 'player2',
+  name: 'Player 2',
+  score: 0
+});
+game.on('changeTurn', lightPlayer);
+game.on('unfill', undo);
+game.on('end', sayWinner);
+game.on('end', blockAll);
+game.on('start', startMatch);
+game.on('win', setScore);
+game.on('fill', blockButton);
+game.on('fill', insertSign);
 
 //ligh the tag <p> of player who has to put the sign
-function lightPlayer() {
+function lightPlayer(player) {
   $('.title').removeClass('active');
-  if(PlayerTurn) {
-    $('#title2').addClass('active');
-  }
-  else {
-    $('#title1').addClass('active');
-  }
-}
-
-//Invert the Player turn
-function invertPlayerTurn() {
-  PlayerTurn = !PlayerTurn;
-  lightPlayer();
+  $('#' + player.id).addClass('active');
 }
 
 //Delete the last insertion
-function undo() {
-  if(lastsign !== null) {
-    $('#'+lastsign).prop('value', '');
-    $('#'+lastsign).prop('disabled', false);
-    counter--;
-    signs[lastsign] = '';
-    invertPlayerTurn();
-  }
-  lastsign = null;
-}
-
-//Check if the match is equal
-function checkPar() {
-  return (counter === 9);
+function undo(e) {
+  $('#' + e.x + e.y).prop('value', '');
+  $('#' + e.x + e.y).prop('disabled', false);
 }
 
 //Say the winner through an alert
-function sayWinner(winner) {
+function sayWinner(e) {
   $('#matchend').show();
-  if(winner) {
+  if(e.player) {
     $('#result').addClass('win');
-    if (PlayerTurn) {
-      $('#result').text('The winner is: Player 1');
-    }
-    else {
-      $('#result').text('The winner is: Player 2');
-    }
+    $('#result').text('The winner is: ' + e.player.name);
   }
   else {
     $('#result').removeClass('win');
@@ -63,28 +49,9 @@ function sayWinner(winner) {
   }
 }
 
-//Delete the signs stored into the array signs[]
-function deleteSigns() {
-  for(var i = 0; i < 9; i++) {
-    signs[i] = '';
-  }
-}
-
-//increase the score
-function updateScore() {
-  if (PlayerTurn) {
-    score[0]++;
-    setScore('player1', 0);
-  }
-  else {
-    score[1]++;
-    setScore('player2', 1);
-  }
-}
-
 //Block the button after his pression
-function blockButton(idbutton) {
-  $('#'+idbutton).prop('disabled', true);
+function blockButton(e) {
+  $('#'+ e.x + e.y).prop('disabled', true);
 }
 
 //Block All the buttons
@@ -97,43 +64,9 @@ function unlockButtons() {
   $('.field').prop('disabled', false);
 }
 
-//Check if a player has won
-function checkStatus() {
-  if (
-    ((signs[0] !== '') && (signs[0] === signs[1]) && (signs[1] === signs[2])) ||
-    ((signs[0] !== '') && (signs[0] === signs[4]) && (signs[4] === signs[8])) ||
-    ((signs[0] !== '') && (signs[0] === signs[3]) && (signs[3] === signs[6])) ||
-    ((signs[1] !== '') && (signs[1] === signs[4]) && (signs[4] === signs[7])) ||
-    ((signs[2] !== '') && (signs[2] === signs[5]) && (signs[5] === signs[8])) ||
-    ((signs[2] !== '') && (signs[2] === signs[4]) && (signs[4] === signs[6])) ||
-    ((signs[3] !== '') && (signs[3] === signs[4]) && (signs[4] === signs[5])) ||
-    ((signs[6] !== '') && (signs[6] === signs[7]) && (signs[7] === signs[8]))
-  ) {
-    sayWinner(true);
-    updateScore();
-    lastsign = null;
-    blockAll();
-  } else if(checkPar()) {
-    sayWinner(false);
-    lastsign = null;
-    blockAll();
-  }
-}
-
 //Insert the sign into the button
-function insertSign(index) {
-  counter++;
-  if (PlayerTurn) {
-    invertPlayerTurn();
-    signs[index] = 'O';
-  } else {
-    invertPlayerTurn();
-    signs[index] = 'X';
-  }
-  $('#'+index).val(signs[index]);
-  lastsign = index;
-  blockButton(index);
-  checkStatus();
+function insertSign(e) {
+  $('#' + e.x + e.y).val(e.sign);
 }
 
 //reset the buttons' values
@@ -144,34 +77,40 @@ function resetFields() {
 //start a new match
 function startMatch() {
   $('#matchend').hide();
-  counter = 0;
   resetFields();
-  deleteSigns();
   unlockButtons();
-  lastsign = null;
 }
 
 //setScore to ""
-function setScore(player, index) {
-  $('#'+player).text(score[index]);
-}
-
-//delete score and start a new match
-function resetAll() {
-  score[0] = 0;
-  setScore('player1', 0);
-  score[1] = 0;
-  setScore('player2', 1);
-  startMatch();
+function setScore() {
+  $('#player1').text(game.players[0].score);
+  $('#player2').text(game.players[1].score);
 }
 
 $(document).ready(function(){
-  lightPlayer();
-  startMatch();
-  setScore('player1', 0);
-  setScore('player2', 1);
   resizeButtons();
   $( window ).resize(function() {
     resizeButtons();
   });
+
+  $('.field').on('click', function(e) {
+    game.insert(Number(this.id[0]), Number(this.id[1]));
+  });
+
+  $('.undo').on('click', function() {
+    game.undo();
+  });
+
+  $('.newmatch').on('click', function() {
+    game.start();
+  });
+
+  $('.reset').on('click', function() {
+    game.players[0].score = 0;
+    game.players[1].score = 0;
+    setScore();
+    game.start();
+  });
+
+  game.start();
 });
